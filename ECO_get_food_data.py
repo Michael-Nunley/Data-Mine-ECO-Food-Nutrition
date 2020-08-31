cfadaa9f7b82a098a2d2cb85622a1e5db1de8a91
Michael Nunley <michael.ray.nunley@gmail.com>
2020-08-31 02:35:43 -0700

    Updated for 9.0 plus formating and info
    Fixed names; changed the DisplayName to LocDisplayName
    And I added some measures to the spreadsheet:
    'Nutrition', Balance Multiplier, and Skill points per day.

--- ECO_get_food_data.py
+++ ECO_get_food_data.py
@@ -19,30 +19,49 @@ def main():
 	workbook = xlsxwriter.Workbook('fooddata.xlsx')
 	worksheet = workbook.add_worksheet()
 
+	numformat = workbook.add_format({'num_format': '###.##0'})
+
 	for idx, text in enumerate(foodandseedfilecontent):
 		try:
-			namemach = re.search(r"DisplayName .*\"(.*)\"",text)
-			worksheet.write(idx,0, namemach[1])
+			namemach = re.search(r"LocDisplayName.*\"(.*)\"",text)
+			worksheet.write(idx+1,0, namemach[1])
 
 			caloriematch = re.search(r"Calories.*n (-?\d{1,4})\;",text)
-			worksheet.write(idx,1, caloriematch[1])
+			worksheet.write(idx+1,1, int(caloriematch[1]))
 
 			carbsmatch = re.search(r"Carbs = (\d{1,4})",text)
-			worksheet.write(idx,2, carbsmatch[1])
+			worksheet.write(idx+1,2, int(carbsmatch[1]) if not int(carbsmatch[1]) == None else 0)
 
 			fatmatch = re.search(r"Fat = (\d{1,4})",text)
-			worksheet.write(idx,3, fatmatch[1])
+			worksheet.write(idx+1,3, int(fatmatch[1]) if not int(fatmatch[1]) == None else 0)
 
 			protmatch = re.search(r"Protein = (\d{1,4})",text)
-			worksheet.write(idx,4, protmatch[1])
+			worksheet.write(idx+1,4, int(protmatch[1]) if not int(protmatch[1]) == None else 0)
 
 			vitaminsmatch = re.search(r"Vitamins = (\d{1,4})",text)
-			worksheet.write(idx,5, vitaminsmatch[1])
+			worksheet.write(idx+1,5, int(vitaminsmatch[1]) if not int(vitaminsmatch[1]) == None else 0)
+
+			worksheet.write_formula(idx+1,6, '=SUM(C{}:F{})'.format(idx+2,idx+2),numformat)
+
+			worksheet.write_formula(idx+1,7, '=G{}/(MAX(C{}:F{})*4)*2'.format(idx+2,idx+2,idx+2),numformat)
+
+			worksheet.write_formula(idx+1,8, '=((G{}*B{})/B{})*H{}+12'.format(idx+2,idx+2,idx+2,idx+2),numformat)
 		except Exception as e:
 			print(text)
 			print(e)
 			raise
 
+	#foodandseedfilecontent.length()
+	worksheet.add_table('A1:I{}'.format(len(foodandseedfilecontent)),{'columns': [{'header': 'Name'},
+                                          										  {'header': 'Calories'},
+																				  {'header': 'Carbs'},
+                                          										  {'header': 'Fat'},
+                                          										  {'header': 'Protein'},
+                                          										  {'header': 'Vitamins'},
+																				  {'header': 'Nutrition'},
+																				  {'header': 'Balance Mult.'},
+																				  {'header': 'Skill points/day'},
+                                          										  ]})
 	workbook.close()
 
 if __name__ == '__main__':
